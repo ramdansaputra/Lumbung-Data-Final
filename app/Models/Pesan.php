e28028c28b2b2c77fc9d542e3070e673c785bae7
Ramdan Dwi Saputra <ramdandsaputra@gmail.com>
2026-04-13 16:42:24 +0700

    Update Pesan.php

--- app/Models/Pesan.php
+++ app/Models/Pesan.php
@@ -12,12 +12,12 @@ class Pesan extends Model
 
     // Relasi Pengirim
     public function pengirim() {
-        return $this->belongsTo(User::class, 'pengirim_id');
+        return $this->belongsTo(Users::class, 'pengirim_id');
     }
 
     // Relasi Penerima
     public function penerima() {
-        return $this->belongsTo(User::class, 'penerima_id');
+        return $this->belongsTo(Users::class, 'penerima_id');
     }
 
     // Threading (Balasan)
