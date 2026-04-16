b9359f828240c388e6dc725f3682759a72c82293
Bagasss08 <bwidyastama@gmail.com>
2026-04-16 17:39:05 +0700

    perbaikan migrate

--- database/migrations/2026_04_16_162056_update_asal_dana_kab_kota_in_program_table.php
+++ database/migrations/2026_04_16_162056_update_asal_dana_kab_kota_in_program_table.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        // Gabungkan nilai 'Kab' dan 'Kota' menjadi 'Kab/Kota'
-        DB::table('program')
-            ->whereIn('asal_dana', ['Kab', 'Kota'])
-            ->update(['asal_dana' => 'Kab/Kota']);
+        if (Schema::hasColumn('program', 'sumber_dana')) {
+            DB::table('program')
+                ->whereIn('sumber_dana', ['Kab', 'Kota'])
+                ->update(['sumber_dana' => 'Kab/Kota']);
+        }
     }
 
     public function down(): void {
-        // Tidak bisa dikembalikan secara akurat karena
-        // tidak tahu mana yang asalnya 'Kab' dan mana 'Kota'
-        // Biarkan kosong atau log saja
+        //
     }
 };
